2f380ba2bf38b788967b5ff926b6d1cda4ef2d78
KristinaBalashova <infernalmajesty1350@gmail.com>
2023-01-06 18:53:02 +0100

    added i118n library and errors display

--- src/init.js
+++ src/init.js
@@ -1,10 +1,30 @@
 import onChange from 'on-change';
 import * as yup from 'yup';
+import i18next from 'i18next';
+import resources from './locales/index.js';
 
 export default () => {
+  const i18nextInstance = i18next.createInstance();
+
+  i18nextInstance.init({
+    lng: 'en',
+    debug: true,
+    resources,
+  });
+
+  yup.setLocale({
+    mixed: {
+      notOneOf: i18nextInstance.t('errors.alreadyExist'),
+    },
+    string: {
+      required: i18nextInstance.t('errors.empty'),
+      url: i18nextInstance.t('errors.notValid'),
+    },
+  });
+
   const state = {
     form: {
-      valid: false,
+      valid: null,
       link: '',
     },
     errors: [],
@@ -12,13 +32,13 @@ export default () => {
   };
 
   const watchedState = onChange(state, async (route, value, previousValue) => {
-    console.log('OnChange');
+    console.log(state.form.valid);
     const form = document.querySelector('.rss-form');
     const input = document.querySelector('input');
-    console.log('added links: ', state.addedLinks);
-    // console.log('value', value);
+    const feedback = document.querySelector('.feedback');
     if (value === false) {
       input.classList.add('is-invalid');
+      feedback.innerHTML = state.errors;
     } else if (value === true) {
       input.classList.remove('is-invalid');
       form.reset();
@@ -33,16 +53,16 @@ export default () => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const value = formData.get('url');
-    watchedState.form.link = value;
+    state.form.link = value;
     schema.validate(watchedState.form.link)
       .then((link) => {
         console.log('validated');
         watchedState.form.valid = true;
         state.errors = null;
-        watchedState.addedLinks.push(link);
+        state.addedLinks.push(link);
       })
       .catch((error) => {
-        state.errors = error.name;
+        state.errors = error
         watchedState.form.valid = false;
       });
   });
